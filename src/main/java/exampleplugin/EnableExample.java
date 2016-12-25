e91e0be90887edb5df48bc09fcd71d1acbf62882
nexusA <dongkyu91622@gmail.com>
2016-12-25 18:23:22 +0900

    on Enable -메세지 띄우기 -- 이벤트 리스너 등록

--- src/main/java/exampleplugin/EnableExample.java
+++ src/main/java/exampleplugin/EnableExample.java
@@ -1,7 +1,19 @@
 package exampleplugin;
 
+import cn.nukkit.event.Listener;
 import cn.nukkit.plugin.PluginBase;
 
-public class EnableExample extends PluginBase {
+public class EnableExample extends PluginBase implements Listener {
 
+	@Override
+	// 플러그인이 활성화
+	public void onEnable() {
+
+		// registerEvents -> 서버에서 일어나는 이벤트를 건네받는 Listener를 등록함
+		this.getServer().getPluginManager().registerEvents(this, this);
+
+		// plugin Enable라는 메세지를 콘솔창에 띄움
+		this.getLogger().info("plugin Enable!");
+
+	}
 }
